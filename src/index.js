4b77b9176d25c710391bb9e77f3f225051428a4d
DmytroVoitovych <begunec23@gmail.com>
2022-11-21 00:37:17 +0200

    test0000

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <ThemeProvider theme={theme}>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-          <BrowserRouter basename="/index.html">
+          <BrowserRouter basename="/">
             <App />
           </BrowserRouter>
         </PersistGate>
